0b612e7c851cb07fbf3f8b63729928d402f9d5a5
quynhngo10 <ngothiquynh1411@gmail.com>
2024-05-07 20:55:57 +0700

    Update product.route.js

--- apps/server/src/routes/v1/product.route.js
+++ apps/server/src/routes/v1/product.route.js
@@ -6,18 +6,18 @@ const productController = require('../../controllers/product.controller');
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 2 * 1024 * 1024, // 2 MB
+    fileSize: 10 * 1024 * 1024, // 2 MB
   },
 });
 
 const router = express.Router();
 
-router.route('/').post(upload.array('file', 5), productController.createProduct).get(productController.getProducts);
+router.route('/').post(upload.array('file', 10), productController.createProduct).get(productController.getProducts);
 
 router
   .route('/:productId')
   .get(productController.getProduct)
-  .patch(upload.array('file', 5), productController.updateProduct)
+  .patch(upload.array('file', 10), productController.updateProduct)
   .delete(productController.deleteProduct);
 
 module.exports = router;
